c8c7b72cd300d96bccd1422974a03fadef1be22b
praveen2jan <praveen1668301@gmail.com>
2021-02-03 15:30:24 +0530

    more codes

--- src/main/java/Arrays/KadaneAlgo.java
+++ src/main/java/Arrays/KadaneAlgo.java
@@ -1,4 +1,18 @@
 package Arrays;
 
 public class KadaneAlgo {
+    public static void main(String[] args) {
+        int num[] = {1,2,3,-2,5};
+
+        int maxSoFar=0;
+        int maxEndingHere =0;
+        for(int i=0;i<num.length;i++){
+            maxEndingHere = maxEndingHere + num[i];
+            if(maxSoFar<maxEndingHere)
+                maxSoFar=maxEndingHere;
+            if(maxEndingHere<0)
+                maxEndingHere=0;
+        }
+        System.out.println(maxSoFar);
+    }
 }
